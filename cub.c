a3cdfb01331f3c509a602a1936a43971fa6c1ed7
elmejhed-ahmed <ael-mejh@student.1337.ma>
2024-09-22 17:34:04 +0100

    cub

--- cub.c
+++ cub.c
@@ -6,7 +6,7 @@
 /*   By: ael-mejh <ael-mejh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 09:23:04 by anqabbal          #+#    #+#             */
-/*   Updated: 2024/09/21 17:52:00 by ael-mejh         ###   ########.fr       */
+/*   Updated: 2024/09/22 17:32:24 by ael-mejh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,25 +28,77 @@ int check_extention(char *str)
 int pars_color_floor_ceiling(char *str, t_texture *texture, int j)
 {
     int i;
+    int k;
+    char **new;
+    char *trim;
 
-    i = j;
-    while(str[i] == ' ')
-        i++;
-    while (str[i] != '\0')
+    i = j + 1;
+    printf("%s", &str[i]);
+    trim = ft_strtrim(&str[i],"\n");
+    i = 0;
+    if (trim[i] == ',')
+        return (printf("Errsor: In111111 \n"), 1);
+    // while (str[k] != '\0' &&  str[k] != '\n')
+    // {
+    //     if (str[k] == ' ')
+    //     {
+    //         k++;
+    //         continue;
+    //     }
+    //     if ((str[k] == ',' && str[k + 1] == ','))
+    //         return (printf("Error: Invalid character '%c' at position %d\n", str[k], k), 1);
+    //     if (str[k] == ',' && (str[k + 1] == '\n' || str[k + 1] == '\0'))
+    //         return (printf("Errsor: In \n"), 1);
+    //     k++;
+    // }
+    new = ft_split(&trim[i], ',');
+    k = 0;
+    while (new[k])
     {
-        if (!ft_isdigit(str[i]) && str[i] != ',' && str[i] != ' ' && str[i] != '\n') {
-            printf("Error: Invalid character '%c' at position %d\n", str[i], i);
-            return 1;
+        int o = 0;
+        trim = ft_strtrim(new[k]," ");
+        printf("trim ====>> %s\n", trim);
+        while (trim[o] && trim[o] != '\n')
+        {
+            if (trim[o] == ' ')
+                return (printf("error\n\n\n"), 1);
+            o++;
         }
-        i++;
-    }
-    while (str[j])
-    {
-        
+        free(trim);
+        printf("[%s] |\n", new[k]);
+        k++;
     }
     
-    printf("%s\n", str);
-    texture = NULL;
+    // k = i;
+    // while (str[i] != '\0' &&  str[i] != '\n')
+    // {
+    //     if (!ft_isdigit(str[i]) && str[i] != ',' && str[i] != ' ') {
+    //         printf("Error: Invalid character '%c' at position %d\n", str[i], i);
+    //         return 1;
+    //     }
+    //     i++;
+    // }
+    // int p = 0;
+    // while (str[k] != '\0' &&  str[k] != '\n')
+    // {
+    //     int nu = 0;
+    //     while (str[k + nu] != '\0' && str[k + nu] != ',' &&  str[k + nu] != '\n')
+    //     {
+    //         if (str[k + nu] == ',')
+    //             p++;
+    //         nu++;
+    //     }
+    //     if (nu > 3)
+    //         return (printf("len len len len number \n"), 1);
+    //     k += nu;
+    //     if (str[k] == ',')
+    //         k++;
+    // }
+    // printf("---------------->>    num ==== {%d}\n", p);
+    if (str[j] == 'F')
+    {
+        texture->F[0] = 225;
+    }
     return 0;
 }
 
@@ -57,7 +109,7 @@ int go_to_check(char *str, t_texture *texture, int i)
     int j = i;
     if ((Fl || Ce ) && (str[i + 1] == ' '))
     {
-        if (pars_color_floor_ceiling(str, texture, i + 1))
+        if (pars_color_floor_ceiling(str, texture, i))
             return 1;
     }
     else if (((N || S || W || E || Fl || Ce )) && (str[i + 2] == ' '))
@@ -66,16 +118,16 @@ int go_to_check(char *str, t_texture *texture, int i)
         while(str[i] == ' ')
             i++;
         if (str[j] == 'N' && str[j + 1] == 'O')
-            texture->NO = ft_strdup(&str[i]);
+            texture->NO = ft_strdup1(&str[i]);
         if (str[j] == 'S' && str[j + 1] == 'O')
-            texture->SO = ft_strdup(&str[i]);
+            texture->SO = ft_strdup1(&str[i]);
         if (str[j] == 'W' && str[j + 1] == 'E')
-            texture->WE = ft_strdup(&str[i]);
+            texture->WE = ft_strdup1(&str[i]);
         if (str[j] == 'E' && str[j + 1] == 'A')
-            texture->EA = ft_strdup(&str[i]);
+            texture->EA = ft_strdup1(&str[i]);
     }
     else 
-        return (printf("error\nerror in direction"), 1);
+        return (printf("error\nerror in direction -> %s", str), 1);
     return 0;
 }
 
@@ -132,10 +184,10 @@ int main(int ac, char **av)
         return (write(0, "error\ninvalid extention\n", 24), 1);
     if (read_file(av, &cub, &texture))
         return (1);
-    // printf("NO ---> %s\n", texture.NO);
-    // printf("SO ---> %s\n", texture.SO);
-    // printf("WE ---> %s\n", texture.WE);
-    // printf("EA ---> %s\n", texture.EA);
+    printf("NO ---> [[[%s]]]\n", texture.NO);
+    printf("SO ---> [[[%s]]]\n", texture.SO);
+    printf("WE ---> [[[%s]]]\n", texture.WE);
+    printf("EA ---> [[[%s]]]\n", texture.EA);
     // printf("F ---> %s\n", texture.F);
     // printf("C ---> %s\n", texture.C);
     return 0;
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ael-mejh <ael-mejh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 20:09:28 by anqabbal          #+#    #+#             */
-/*   Updated: 2024/09/19 18:17:04 by ael-mejh         ###   ########.fr       */
+/*   Updated: 2024/09/22 13:30:37 by ael-mejh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,4 +77,8 @@ char	*my_strjoin(char *s1, char *s2);
 void	*my_memcpy(void *dst, const void *src, size_t n);
 char	*to_join(char *s1, char *s2, int indice);
 void	*my_calloc(size_t count, size_t size);
+
+/*-----------------------------*/
+char	*ft_strdup1(const char *s1);
+
 #endif
